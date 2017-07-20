8afaeafb587f382a91ff24b0ef8c93c4371202a7
Mark Ratliff <mark.s.ratliff@gmail.com>
2017-07-20 13:09:28 -0400

    Adjusting sitecopier to deal with nil returns

--- sitecopier.rb
+++ sitecopier.rb
@@ -17,10 +17,14 @@ class SiteCopier
   	@websites.each do |element|
   		url = "http://api.page2images.com/restfullink?p2i_url=" + element + "&p2i_key=5b88106f9a7ab1c9&p2i_size=750x750"
   		response = HTTParty.get url
-		sleep 3
-		body = JSON.parse response.body
-  		@images << body["image_url"]
-  	end
+		  sleep 1.5
+		  body = JSON.parse response.body
+        if body["status"] == "error"
+          @images << "SSL site detected"
+        else
+      		@images << body["image_url"]
+        end
+    end
 
   	@images
 
@@ -28,15 +32,15 @@ class SiteCopier
 
 end
 
-arr = [
-  "http://devbootcamp.com/",
-  "http://www.hackreactor.com/",
-  "https://wyncode.co/",
-  ["https://www.coursereport.com/", "http://www.skilledup.com/", "https://www.becasted.com", "https://news.ycombinator.com/"].sample
-]
+# arr = [
+#   "http://devbootcamp.com/",
+#   "http://www.hackreactor.com/",
+#   "https://wyncode.co/",
+#   ["https://www.coursereport.com/", "http://www.skilledup.com/", "https://www.becasted.com", "https://news.ycombinator.com/"].sample
+# ]
 
-image_grabber = SiteCopier.new(arr)
+# image_grabber = SiteCopier.new(arr)
 
-image_grabber.get_screenshots
+# image_grabber.get_screenshots
 
-p image_grabber.images
+# p image_grabber.images
